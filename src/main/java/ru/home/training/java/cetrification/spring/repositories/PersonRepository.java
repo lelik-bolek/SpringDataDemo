6c0c270b6529be3195084b65dfa0c0bcf98e05d7
Lelikov Alexander <lelikov30@gmail.com>
2025-05-16 12:53:59 +0300

    Добавлены новые поисковые методы
    Добавили 4 поисковых метода:
    findByName(String name);
    findByAgeBetween(int ageFrom, int ageTo);
    findByAddressCity(String city);
    
    Решили проблему "N+1" (Дополнительные запросы)
    за счет создания метода findPersonsWhithAddresses()

--- src/main/java/ru/home/training/java/cetrification/spring/repositories/PersonRepository.java
+++ src/main/java/ru/home/training/java/cetrification/spring/repositories/PersonRepository.java
@@ -1,9 +1,21 @@
 package ru.home.training.java.cetrification.spring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ru.home.training.java.cetrification.spring.model.Person;
+import java.util.List;
+
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByName(String name);
+
+    List<Person> findByAgeBetween(int ageFrom, int ageTo);
+
+    List<Person> findByAddressCity(String city);
+
+    @Query("SELECT p FROM Person p LEFT JOIN FETCH p.address")
+    List<Person> findPersonsWhithAddresses();
 }
 
